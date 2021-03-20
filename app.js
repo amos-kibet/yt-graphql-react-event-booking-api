c610cd315f4ee8e08cda7b0b71d381f53831f44f
amos-kibet <kibetamos97@gmail.com>
2021-03-20 16:46:03 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic', 'Cooking', 'Coding'];
+        },
+        createEvent: (args) => {
+            return args.name;
+        }
+    },
+    graphiql: true,
+}));
 app.listen(3000);
+console.log("App started...")
